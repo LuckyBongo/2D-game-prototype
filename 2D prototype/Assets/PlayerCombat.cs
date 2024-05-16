76beaa358e6e310a7b4735be3ca7fe490f6ba71a
DoodlesPlus <ironfist200413@gmail.com>
2024-05-15 18:35:28 -0700

    AttackEnemy

--- 2D prototype/Assets/PlayerCombat.cs
+++ 2D prototype/Assets/PlayerCombat.cs
@@ -22,8 +22,7 @@ public class PlayerCombat : MonoBehaviour
 
     void Attack()
     {
-        animator.SetTrigger("Attack1");
-
+        
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, AttackRange, enemyLayers);
 
         foreach (Collider2D enemy in hitEnemies)
